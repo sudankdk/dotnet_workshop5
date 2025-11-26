3d59bd94f5db73cec6893da61e36a4775ba301d6
sudankdk <sudankhadka345@gmail.com>
2025-11-26 11:53:43 +0545

    task 2 done

---
+++ Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Task2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var car = new Car
+        {
+            Brand = "Toyota",
+            Speed = 120,
+            Seat = "Leather"
+        };
+
+        car.DisplayInfo();
+
+        var bike = new Motorcycle
+        {
+            Brand = "Yamaha",
+            Speed = 150,
+            Engine = "250cc"
+        };
+
+        bike.DisplayInfo();
+
+    }
+}
